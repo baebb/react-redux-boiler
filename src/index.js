af75c1dde47fed77dea676d3748718c60278db85
Ross Dyson <ross@soundninja.com>
2017-02-23 01:10:58 -0800

    Adding redux-thunk and support for redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Router, browserHistory} from 'react-router';
+import Thunk from 'redux-thunk';
 
 import Routes from './routes';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = [Thunk];
+
+const store = createStore(reducers, composeEnhancers(
+  applyMiddleware(...middleware)
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} routes={Routes}/>
   </Provider>
   , document.querySelector('.container'));
