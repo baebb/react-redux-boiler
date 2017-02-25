7d6fc07ac5657308d18828cc0c1a1fa37b36b452
Ross Dyson <ross@soundninja.com>
2017-02-25 00:38:18 -0800

    react router syntax update

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Route, IndexRoute} from 'react-router';
+import {Route, Router, IndexRoute} from 'react-router';
 
 import App from './components/app';
 import Home from './components/home';
 
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={Home}/>
-  </Route>
+  <Router>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+    </Route>
+  </Router>
 )
