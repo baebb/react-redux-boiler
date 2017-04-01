8ed91553e190aefa004ec4cf74067398d79b10e6
Ross Dyson <dyson.ross@gmail.com>
2017-04-01 21:24:46 +1100

    adding webpack build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,98 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+
+const sourcePath = path.join(__dirname);
+const jsSourcePath = path.join(__dirname, './src');
+const buildPath = path.join(__dirname, './build');
+const imgPath = path.join(__dirname, './img');
+
+// Common plugins
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(nodeEnv),
+    },
+  }),
+  new webpack.NamedModulesPlugin(),
+  // new webpack.LoaderOptionsPlugin({
+  //   options: {
+  //     postcss: [
+  //       autoprefixer({
+  //         browsers: [
+  //           'last 3 version',
+  //           'ie >= 10',
+  //         ],
+  //       }),
+  //     ],
+  //     context: sourcePath,
+  //   },
+  // }),
+];
+
+// Common rules
+const rules = [
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: [
+      'babel-loader',
+    ],
+  },
+  {
+    test: /\.(png|gif|jpg|svg)$/,
+    include: imgPath,
+    use: 'url-loader?limit=20480&name=assets/[name]-[hash].[ext]',
+  },
+];
+
+if(nodeEnv == 'production') {
+  plugins.push(
+    new HtmlWebpackPlugin({
+      template: path.join(sourcePath, 'index.html'),
+      path: buildPath,
+      filename: 'index.html',
+    })
+  )
+} else {
+  
+}
+
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
-    path: __dirname,
+    path: buildPath,
     publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      {
+        test: /\.(png|gif|jpg|svg)$/,
+        include: imgPath,
+        use: 'url-loader?limit=20480&name=assets/[name]-[hash].[ext]',
+      },
+    ]
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
+    modules: [
+      path.resolve(sourcePath, 'node_modules'),
+      jsSourcePath,
+    ],
   },
+  plugins,
   devServer: {
     historyApiFallback: true,
     contentBase: './'
