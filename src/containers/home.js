23b572870b79da721a4b510817232221b6478163
Ross Dyson <dyson.ross@gmail.com>
2017-05-08 23:55:17 +1000

    adding number input

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,42 +1,69 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Grid, Row, Col, Button} from 'react-bootstrap';
+import {Grid, Row, Col, Button, FormGroup, InputGroup, FormControl} from 'react-bootstrap';
 import {Link} from 'redux-little-router';
 
 import {navigateAbout, navigateCount} from '../actions/index';
 
 class Home extends React.Component {
-  render() {
-    return (
-      <Grid>
-        <Row>
-          <Col xs={12} sm={6} smOffset={3}>
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: ''
+		}
+	}
+	
+	handleChange(e) {
+		this.setState({...this.state, value: e.target.value});
+	}
+	
+	render() {
+		return (
+			<Grid>
+				<Row>
+					<Col xs={12} sm={6} smOffset={3}>
 						<div className="text-center">
 							<h1 className="text-center">Home</h1>
-							{/*<Link href={{*/}
-							{/*pathname: '/count',*/}
-							{/*query: {*/}
-							{/*test: 'ing'*/}
-							{/*}*/}
-							{/*}}>*/}
-							{/*<Button>Count</Button>*/}
-							{/*</Link>*/}
-							<Button
-								onClick={() => this.props.dispatch(navigateCount(3))}
-							>
-								Count to 3
-							</Button>
-							<Button
-								onClick={() => this.props.dispatch(navigateAbout())}
-							>
-								About
-							</Button>
+							<div>
+								<form>
+									<FormGroup>
+										<InputGroup>
+											<InputGroup.Addon>Query</InputGroup.Addon>
+											<FormControl
+												type="text"
+												value={this.state.value}
+												placeholder="Enter number"
+												onChange={this.handleChange.bind(this)}
+											/>
+											<InputGroup.Button>
+												<Button
+													onClick={() => this.props.dispatch(navigateCount(this.state.value))}
+												>
+													Go
+												</Button>
+											</InputGroup.Button>
+										</InputGroup>
+									</FormGroup>
+								</form>
+								{/*<Button*/}
+								{/*onClick={() => this.props.dispatch(navigateCount(3))}*/}
+								{/*>*/}
+								{/*Count to 3*/}
+								{/*</Button>*/}
+							</div>
+							<div>
+								<Button
+									onClick={() => this.props.dispatch(navigateAbout())}
+								>
+									About
+								</Button>
+							</div>
 						</div>
-          </Col>
-        </Row>
-      </Grid>
-    )
-  }
+					</Col>
+				</Row>
+			</Grid>
+		)
+	}
 }
 
 export default connect()(Home);
