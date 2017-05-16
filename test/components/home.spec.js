ac9f11bdc59d63685f286a27750598ba4e12c57e
justrossthings <dyson.ross@gmail.com>
2017-05-17 00:14:05 +1000

    scaffolding

---
+++ test/components/home.spec.js
@@ -0,0 +1,16 @@
+import expect from 'expect';
+import {mount, shallow} from 'enzyme'
+
+import Home from '../../src/containers/home';
+
+describe('Home' , () => {
+  let component;
+
+  beforeEach(() => {
+    component = mount(Home);
+  });
+
+  // it('renders something', () => {
+  //   expect(component).to.exist;
+  // });
+});
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,40 +1,17 @@
-import jsdom from 'jsdom';
-import jquery from 'jquery';
-import TestUtils from 'react-addons-test-utils';
-import ReactDOM from 'react-dom';
-import chai, {expect} from 'chai';
-import chaiJquery from 'chai-jquery';
-import React from 'react';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+const { JSDOM } = require('jsdom');
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
 
-import reducers from '../src/reducers';
-
-//set up testing enviroment to run like a browser in command line
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
-const $ = jquery(global.window);
-
-//build 'renderComponent' helper that should render a given react class
-function renderComponent(ComponentClass, props, state) {
-  const componentInstance = TestUtils.renderIntoDocument(
-      <Provider store={createStore(reducers, state)}>
-        <ComponentClass {...props} />
-      </Provider>
-  );
-
-  return $(ReactDOM.findDOMNode(componentInstance)); //produces HTML of element
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
 }
 
-//build help for simulating events
-$.fn.simulate = function(eventName, value) {
-  if (value) {
-    this.val(value);
-  }
-  TestUtils.Simulate[eventName](this[0]);
-}
-
-//set up chai-jquery
-chaiJquery(chai, chai.util, $);
-
-export {renderComponent, expect}
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js'
+};
+copyProps(window, global);
