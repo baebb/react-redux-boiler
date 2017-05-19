f372a36f2a412eb2ad70d4c024035b71684f9084
justrossthings <dyson.ross@gmail.com>
2017-05-19 20:40:39 +1000

    Cleaning bootstrap import

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {Provider} from 'react-redux';
 import configureStore from './store.js';
 import {initializeCurrentLocation, RouterProvider} from 'redux-little-router';
 
-import '../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss';
+import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import './scss/app.scss';
 
 const store = configureStore();
